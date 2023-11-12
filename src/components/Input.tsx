d054a7d71a5f9569d1d8a28aee8a5c54f3e1e69f
Shariq Asadi <shariq.asadi@gmail.com>
2023-11-12 14:18:22 +0500

    Update Input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,20 @@
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label: string
+}
+
+export default function Input(props: InputProps) {
+  return (
+    <div role="group" className="flex flex-col gap-2">
+      <label
+        htmlFor={props.id}
+        className="text-sm font-bold uppercase leading-normal tracking-[3.5px] text-smokey-grey"
+      >
+        {props.label}
+      </label>
+      <input
+        {...props}
+        className="max-w-[160px] rounded-lg border border-solid border-light-grey px-6 py-3 text-[32px] font-bold tracking-[0.32px] text-off-black  focus:border focus:border-purple focus:caret-purple focus-visible:border focus-visible:border-purple focus-visible:caret-purple focus-visible:outline-none"
+      />
+    </div>
+  )
+}
